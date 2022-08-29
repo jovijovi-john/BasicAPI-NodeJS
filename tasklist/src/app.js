ea877c6b416d53108d5b385e4111e7c14c8fd220
jovijovi-john <jovijovi2019@hotmail.com>
2022-08-29 11:46:43 -0300

    configurando eslint e prettier

--- tasklist/src/app.js
+++ tasklist/src/app.js
@@ -1,5 +1,5 @@
-import express from "express";
-import routes from "./routes";
+import express from 'express';
+import routes from './routes';
 
 class App {
     constructor() {
@@ -10,10 +10,10 @@ class App {
     }
 
     middlewares() {
-        this.server.use(express.json())
+        this.server.use(express.json());
     }
 
-    routes(){
+    routes() {
         this.server.use(routes);
     }
 }
--- tasklist/src/routes.js
+++ tasklist/src/routes.js
@@ -1,9 +1,7 @@
-import { Router } from "express";
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get("/teste", (req, res) => {
-    return res.json({message: "ok"})
-});
+routes.get('/teste', (req, res) => res.json({ message: 'ok' }));
 
 export default routes;
