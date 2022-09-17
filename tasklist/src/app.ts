b01483d09428114c8e39d29b7df8e106de42eab5
John <jovijovi2019@hotmail.com>
2022-09-17 18:30:29 -0300

    Finalizando Projeto

--- tasklist/src/app.ts
+++ tasklist/src/app.ts
@@ -1,15 +1,25 @@
 import express from 'express';
 import routes from './routes';
 
+import cors from "cors";
+
 class App {
+
+    server: any;
+
     constructor() {
         this.server = express();
+        this.server.listen(4000, () => {
+            console.log("Server na porta http://localhost:4000")
+        });
 
         this.middlewares();
         this.routes();
+
     }
 
     middlewares() {
+        this.server.use(cors())
         this.server.use(express.json());
     }
 
--- tasklist/src/routes.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-
-const routes = new Router();
-
-routes.get('/teste', (req, res) => res.json({ message: 'ok' }));
-
-export default routes;
